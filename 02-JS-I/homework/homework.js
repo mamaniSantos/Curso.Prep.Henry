8770e575a13aaed5a644132feec098363f1de446
martinMamani <mpm.mamani@gmail.com>
2020-08-25 16:37:13 -0300

    quinto commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -97,7 +97,7 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(num / 2 == 0){
+  if(num % 2 == 0){
     return true;
   }else{
     return false;
@@ -108,7 +108,7 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(num / 2 != 0){
+  if(num % 2 != 0){
     return true;
   }else{
     return false;
