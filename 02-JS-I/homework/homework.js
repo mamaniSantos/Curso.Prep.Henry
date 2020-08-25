4ba39fd96a2be6de29af49f4b5cee955dc49fc08
martinMamani <mpm.mamani@gmail.com>
2020-08-25 15:52:29 -0300

    segundo commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,33 +28,39 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  return str;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+  return x+y;
 }
 
 function resta(x, y) {
   // Resta "x" de "y" y devuelve el valor
   // Tu código:
+  return y-x;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
+  return x*y;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
+  return x/y;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+  return x==y;
 }
 
 function tienenMismaLongitud(str1, str2) {
